e8f557a9d3a5698529fc8cd2bd624e5d13d336d5
vasireddyrajesh <vrajesh.myreg@gmail.com>
2026-04-05 15:45:22 +0530

    Delete Rajesh_Vasireddy/hackathon directory

--- Rajesh_Vasireddy/hackathon/integrations/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Third-party integrations package."""
-
-from .slack_client import SlackClient
-from .jira_client import JiraClient
-
-__all__ = ["SlackClient", "JiraClient"]
--- Rajesh_Vasireddy/hackathon/orchestrator/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Orchestrator package."""
-
-from .runner import run_incident_pipeline
-from .state import IncidentState
-
-__all__ = ["run_incident_pipeline", "IncidentState"]
